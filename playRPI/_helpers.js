7be5af1e6fd14f2a1ad18c788aaa096a79adc7e8
circus2271 <rodionov134@gmail.com>
2026-06-02 21:55:25 +0300

    fix enormous virtual memory consumption...

--- playRPI/_helpers.js
+++ playRPI/_helpers.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-
+const fetch = require('node-fetch')
 const playlistConfig = require('./_playlistConfig');
 
 // helper functions
